47249046bc54e7daf6538dffe67de04747046de4
George Ho <georgeho1618@gmail.com>
2018-10-13 17:43:24 -0400

    REV: remove tf slim; deprecated

--- download-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATA_DIR=./tmp/data/cifar10
-python models/research/slim/download_and_convert_data.py \
-    --dataset_name=cifar10 \
-    --dataset_dir=${DATA_DIR}
-
-# Now we can easily create a TF-Slim dataset descriptor...
-# https://github.com/tensorflow/models/tree/master/research/slim#creating-a-tf-slim-dataset-descriptor
